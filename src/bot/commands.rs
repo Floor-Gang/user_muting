dc716234a96a4ab89451987c72f8e5a19cb6327b
slycor <matijacavrag94@gmail.com>
2020-09-09 18:17:30 +0200

    - Added: parsing of arguments in mute function - Added: filtering function for tags
    Parsing of input arguments (user, time, reason) is completed. Also,
    a function which filters the different types of tags (@, #, ...) has
    been implemented.

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -1,21 +1,25 @@
 use serenity::{
     prelude::*,
     framework::standard::{
+        Args,
         CommandResult,
         macros::{
             command, group
         }
     },
     model::{
-        channel::Message
+        channel::Message,
+        id::UserId
     }
 };
-use crate::bot::utils::{reply};
+use crate::bot::utils::{reply, filter_tag};
 use crate::bot::DataBase;
 use crate::config::Config;
+use regex::Regex;
+use chrono::Duration;
 
 #[group()]
-#[commands(ping, db_test, prefix)]
+#[commands(ping, db_test, prefix, mute)]
 pub struct Commands;
 
 #[command]
@@ -56,3 +60,158 @@ async fn db_test(ctx: &Context, msg: &Message) -> CommandResult {
     reply(&ctx, &msg, &rows[0].get(0)).await;
     Ok(())
 }
+
+#[command]
+async fn mute(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    // if arg is empty reply it to channel
+    if args.is_empty() {
+        if let Err(why) = msg.channel_id.send_message(&ctx.http,  |m| {
+            m.embed(|embed| {
+                embed.title("mute");
+                embed.description(format!("-mute [userID] [time(optional)] [reason]"));
+                embed.color(0xffa500)
+            });
+            m
+
+        }).await {
+            println!("Failed to send message in #{} because\n{:?}",
+                     msg.channel_id, why
+            );
+        };
+    }
+
+    // Parse first argument
+    let user = args.single::<String>().unwrap_or(String::from(""));
+
+    if user == "" {
+        println!("User not provided!");
+        return Ok(());
+    }
+
+    // get the guild
+    if let Some(guild) = msg.guild(&ctx.cache).await {
+        // Get the user ID from provided tag
+        let uid = UserId(filter_tag(&user.to_owned()));
+
+        if uid == 0 {
+            println!("Something horrible happened!");
+            return Ok(());
+        }
+
+        // Get the member with the given user ID
+        let member = {
+            match ctx.cache.member(guild.id, uid).await {
+                Some(member) => member,
+                None => {
+                    println!("Couldn't find user with id: {}", uid);
+                    return Ok(());
+                },
+            }
+        };
+
+
+        // Parse second argument
+        let mut time = args.single::<String>().unwrap_or(String::from(""));
+
+        if time != "" {
+            // var used for timeout duration
+            let mut sleep_count = Duration::seconds(0);
+
+            // find the time duration
+            let m = Regex::new("[0-9]{1,}").unwrap();
+            let time_dur_borders = m.find(&time);
+
+            // check if we found a time duration
+            if time_dur_borders.is_some() {
+                let time_duration = &time[time_dur_borders.unwrap().start()..
+                                          time_dur_borders.unwrap().end()];
+
+                let i64_time_val = time_duration.parse::<i64>().unwrap_or(0);
+
+                // find the time unit
+                let tu = Regex::new("[A-Za-z]{1,}").unwrap();
+                let time_unit_borders = tu.find(&time);
+
+                // check if we found a time unit and if it is valid
+                if time_unit_borders.is_some() &&
+                   (time_unit_borders.unwrap().end() == time.len()) {
+
+                    let time_unit = &time[time_unit_borders.unwrap().start()..
+                                        time_unit_borders.unwrap().end()];
+
+                    // Calculate the required timeout value
+                    match time_unit {
+                        "s" | "sec"  => sleep_count = Duration::seconds(i64_time_val),
+                        "m" | "min"   => sleep_count = Duration::minutes(i64_time_val),
+                        "h" | "hour" => sleep_count = Duration::hours(i64_time_val),
+                        "d" | "day" => sleep_count = Duration::days(i64_time_val),
+                        "w" | "week" => sleep_count = Duration::weeks(i64_time_val),
+                        _ => {
+                            println!("Wrong time format!");
+                            time = String::from("");
+                            args.rewind();
+                        }
+                    }
+                } else {
+                    // wrong time format, rewind the arguments
+                    println!("Wrong time format provided!");
+                    time = String::from("");
+                    args.rewind();
+                }
+            } else {
+                // wrong time format, rewind the arguments
+                println!("Time was not provided!");
+                time = String::from("");
+                args.rewind();
+            }
+
+        }
+
+        // Parse the input arguments to get the reason variable. If time is not
+        // provided reason starts from second argument.
+        let mut reason = String::from("");
+
+        if (args.len() > 2) || (time == "") {
+            for arg in args.iter::<String>() {
+                reason = reason + &format!(" {}", arg.unwrap_or(String::from("")));
+            }
+            reason = reason.trim().to_string();
+        } else {
+            println!("Reason not provided!");
+            return Ok(());
+        }
+
+        // if time not provided we assume user is muted indefinitely
+        if time == "" {
+            time = String::from("indefinitely");
+        }
+
+        // Reply to the channel from where it was sent.
+        reply(&ctx, &msg, &String::from(format!("Muted user {} for {}! Reason: {}",
+                                        user, time, reason))).await;
+    }
+
+    Ok(())
+}
+
+//TODO
+/*
+#[command]
+async fn unmute(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    if args.is_empty() {
+        if let Err(why) = msg.channel_id.send_message(&ctx.http,  |m| {
+            m.embed(|embed| {
+                embed.title("unmute");
+                embed.description(format!("-mute [userID]"));
+                embed.color(0xffa500)
+            });
+            m
+
+        }).await {
+            println!("Failed to send message in #{} because\n{:?}",
+                     msg.channel_id, why
+            );
+        };
+    }
+    Ok(())
+}*/
--- src/bot/utils.rs
+++ src/bot/utils.rs
@@ -1,5 +1,6 @@
 use serenity::prelude::Context;
 use serenity::model::channel::Message;
+use regex::Regex;
 
 pub(crate) async fn reply(ctx: &Context, msg: &Message, content: &String) {
     if let Err(why) = msg.channel_id.say(&ctx.http, &content).await {
@@ -9,12 +10,43 @@ pub(crate) async fn reply(ctx: &Context, msg: &Message, content: &String) {
     }
 }
 
-/*
-pub(crate) async fn reply_embed<T>(ctx: &Context, msg: &Message, embed: T) {
-    if let Err(why) = msg.channel_id.send_message(&ctx.http, &embed).await {
-        println!("Failed to send message in #{} because\n{:?}",
-                 msg.channel_id, why
-        );
+// FilterTag takes intakes a ID formatted by the Discord client
+// ie <#ID>, <:ID>, or <@ID> and returns ID.
+pub(crate) fn filter_tag(input_tag: &String) -> u64 {
+    let type_tag = &input_tag[1..2];
+    let m;
+    let mut tag = "";
+
+	match type_tag {
+	    // A channel mention
+	    "#" => m = Regex::new("#(.*?)>").unwrap(),
+	    // An emoji
+	    ":" => m = Regex::new(":(.*?)>").unwrap(),
+	// A role or a user mention
+	    "@" => {
+            if &input_tag[2..3] == "&" {
+                m = Regex::new("&(.*?)>").unwrap();
+            } else {
+                m = Regex::new("!(.*?)>").unwrap();
+            }
+        },
+        _ => return input_tag.parse::<u64>().unwrap_or(0)
+	}
+
+    let find_str = m.find(input_tag).unwrap();
+
+	if find_str.start() > 0 {
+		// Remove the last character, which is ">"
+		tag = &input_tag[..find_str.end()-1];
+
+		// If it's not an emoji, because we need the :emoji:<id>
+		if type_tag != ":" {
+			// Remove the first character, which is ["!", "&", "#"]
+			tag = &tag[find_str.start()+1..];
+		} else {
+            tag = &tag[1..];
+        }
     }
+
+	tag.parse::<u64>().unwrap_or(0)
 }
-*/
